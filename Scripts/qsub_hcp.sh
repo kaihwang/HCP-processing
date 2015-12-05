ade919c352882a702db3ad2856c9f42247d4fdac
kai.hwang@gmail.com <kaihwang@nx2.priv>
2015-12-05 10:27:21 -0800

    switch to 3dBandpass, run rest of the subjects for Maxwell.

--- Scripts/qsub_hcp.sh
+++ Scripts/qsub_hcp.sh
@@ -1,10 +1,10 @@
 SCRIPT='/home/despoB/kaihwang/bin/HCP-processing/Scripts'
 DATA='/home/despoB/kaihwang/bin/HCP-processing/Data'
 
-for s in $(cat $DATA/List_of_100_least_movement_subjects); do
+for s in $(cat $DATA/list_of_complete_subjects); do
 	#if [ ! -e "/home/despoB/kaihwang/Rest/Graph/gsetCI_${Subject}.mat" ]; then
-	sed "s/s in 133928/s in ${s}/g" < ${SCRIPT}/HCP_preproc.sh > ~/tmp/s${s}.sh
-	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/s${s}.sh
+	sed "s/s in 100408/s in ${s}/g" < ${SCRIPT}/HCP_preproc.sh > ~/tmp/s${s}.sh
+	qsub -l mem_free=5G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/s${s}.sh
 	#fi
 
 done
