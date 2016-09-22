6a0361fca155eb4ba6d3b93c476a7aa2b046fbf2
Kai Hwang <kai.hwang@gmail.com>
2016-09-22 13:56:59 -0700

    fix if statement.

--- Scripts/HCP_preproc.sh
+++ Scripts/HCP_preproc.sh
@@ -3,7 +3,7 @@ WD='/home/despoB/connectome-data'
 SCRIPTS='/home/despoB/kaihwang/bin/HCP-processing'
 Tha_folder='/home/despoB/connectome-thalamus/connectome'
 
-for s in 100408; do
+for s in 996782; do
 
 	if [ ! -d ${WD}/${s}/ ]; then
 		mkdir ${WD}/${s}/		
@@ -26,7 +26,7 @@ for s in 100408; do
 
 
 	#csf mask
-	if [ ! -e ${WD}/${s}/CSF_mask_ds_erode1x.ni ]; then
+	if [ ! -e ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz ]; then
 		3dcalc -a ${WD}/${s}/aseg+tlrc -prefix ${WD}/${s}/CSF_mask.nii.gz -expr 'amongst(a,4,43)'
 		3dresample -inset ${WD}/${s}/CSF_mask.nii.gz  \
 		-master ${WD}/${s}/brainmask_ds.nii.gz \
@@ -46,66 +46,65 @@ for s in 100408; do
 	fi
 
 
-	## do rfMRI
+
+
+	## do task data
+	for data in tfMRI_EMOTION_LR tfMRI_EMOTION_RL tfMRI_GAMBLING_LR tfMRI_GAMBLING_RL tfMRI_LANGUAGE_LR tfMRI_LANGUAGE_RL tfMRI_MOTOR_LR tfMRI_MOTOR_RL tfMRI_RELATIONAL_LR tfMRI_RELATIONAL_RL tfMRI_SOCIAL_LR tfMRI_SOCIAL_RL tfMRI_WM_LR tfMRI_WM_RL; do
+	
+		if [ ! -d ${WD}/${s}/${data} ]; then
+			mkdir ${WD}/${s}/${data}		
+		fi
+	
+		if [ ! -e ${WD}/${s}/${data}/${data}.nii.gz ]; then
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/${data}.nii.gz ${WD}/${s}/${data}/${data}.nii.gz
+		fi
+		
+		if [ ! -e ${WD}/${s}/${data}/${data}_reg.nii.gz ]; then
+
+			# extract tissue regressors
+			3dmaskave -mask ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_csf.1D
+			3dmaskave -mask ${WD}/${s}/WM_mask_ds_erode1x.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_WM.1D
+			3dmaskave -mask ${WD}/${s}/brainmask_ds.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_WBS.1D	
+			rm ${WD}/${s}/${data}/${data}_motion.1D
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/Movement_Regressors_dt.txt ${WD}/${s}/${data}/${data}_motion.1D
+	
+			#do regression
+			3dTproject \
+			-input ${WD}/${s}/${data}/${data}.nii.gz \
+			-prefix ${WD}/${s}/${data}/${data}_reg.nii.gz \
+			-ort ${WD}/${s}/${data}/${data}_csf.1D \
+			-ort ${WD}/${s}/${data}/${data}_WM.1D \
+			-ort ${WD}/${s}/${data}/${data}_WBS.1D \
+			-ort ${WD}/${s}/${data}/${data}_motion.1D \
+			-automask \
+			-passband 0.009 0.08 \
+			-blur 6 \
+			-polort 2
+		fi
+	
+	done  
+
+		## do rfMRI
 	for data in rfMRI_REST1_LR rfMRI_REST1_RL rfMRI_REST2_LR rfMRI_REST2_RL; do
 
 		if [ ! -d ${WD}/${s}/${data} ]; then
 			mkdir ${WD}/${s}/${data}		
 		fi
 
-		if [ ! -e ${WD}/${s}/${data}/${data}_hp2000_clean_wbsreg.nii.gz ]; then
-			#ln -s ${Tha_folder}/${s}/MNINonLinear/${data}_hp2000_clean_wbsreg.nii.gz ${WD}/${s}/${data}/${data}_hp2000_clean_wbsreg.nii.gz
+		if [ ! -e ${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz ]; then
 			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/${data}_hp2000_clean.nii.gz ${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz
-			
+		fi
+
+		if [ ! -e ${WD}/${s}/${data}/${data}_hp2000_clean_wbsreg.nii.gz ]; then
 			3dmaskave -mask ${WD}/${s}/brainmask_ds.nii.gz -quiet \
 			${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz > ${WD}/${s}/${data}_WBS.1D
 			
-			3dBandpass -input ${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz \
+			3dTproject -input ${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz \
 			-prefix ${WD}/${s}/${data}/${data}_hp2000_clean_wbsreg.nii.gz \
 			-ort ${WD}/${s}/${data}_WBS.1D \
-			-band 0.009 0.08 \
-			-notrans \
+			-passband 0.009 0.08 \
 			-automask
 		fi
 	done 
-
-
-	## do task data
-	# for data in tfMRI_EMOTION_LR tfMRI_EMOTION_RL tfMRI_GAMBLING_LR tfMRI_GAMBLING_RL tfMRI_LANGUAGE_LR tfMRI_LANGUAGE_RL tfMRI_MOTOR_LR tfMRI_MOTOR_RL tfMRI_RELATIONAL_LR tfMRI_RELATIONAL_RL tfMRI_SOCIAL_LR tfMRI_SOCIAL_RL tfMRI_WM_LR tfMRI_WM_RL; do
-
-	# 	if [ ! -d ${WD}/${s}/${data} ]; then
-	# 		mkdir ${WD}/${s}/${data}		
-	# 	fi
-
-	# 	if [ ! -e ${WD}/${s}/${data}/${data}.nii.gz ]; then
-	# 		ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/${data}.nii.gz ${WD}/${s}/${data}/${data}.nii.gz
-	# 	fi
-
-	# 	# extract tissue regressors
-	# 	3dmaskave -mask ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_csf.1D
-	# 	3dmaskave -mask ${WD}/${s}/WM_mask_ds_erode1x.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_WM.1D
-	# 	3dmaskave -mask ${WD}/${s}/brainmask_ds.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_WBS.1D
-
-	# 	#creat symlink for motion regressors
-	# 	if [ ! -e ${WD}/${s}/${data}/${data}_motion.1D ]; then
-	# 		ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/Movement_Regressors_dt.txt ${WD}/${s}/${data}/${data}_motion.1D
-	# 	fi
-
-	# 	#do regression
-	# 	if [ ! -e ${WD}/${s}/${data}/${data}_reg.nii.gz ]; then
-	# 		3dTproject \
-	# 		-input ${WD}/${s}/${data}/${data}.nii.gz \
-	# 		-prefix ${WD}/${s}/${data}/${data}_reg.nii.gz \
-	# 		-ort ${WD}/${s}/${data}/${data}_csf.1D \
-	# 		-ort ${WD}/${s}/${data}/${data}_WM.1D \
-	# 		-ort ${WD}/${s}/${data}/${data}_WBS.1D \
-	# 		-ort ${WD}/${s}/${data}/${data}_motion.1D \
-	# 		-automask \
-	# 		-passband 0.009 0.08 \
-	# 		-blur 6 \
-	# 		-polort 2
-	# 	fi
-
-	# done  
 
 done
--- Scripts/qsub_hcp.sh
+++ Scripts/qsub_hcp.sh
@@ -1,10 +1,11 @@
 SCRIPT='/home/despoB/kaihwang/bin/HCP-processing/Scripts'
 DATA='/home/despoB/kaihwang/bin/HCP-processing/Data'
 
-for s in $(cat $DATA/list_of_complete_subjects); do
+cd /home/despoB/connectome-raw
+for s in $(/bin/ls -d * | grep "[0-9]"); do
 	#if [ ! -e "/home/despoB/kaihwang/Rest/Graph/gsetCI_${Subject}.mat" ]; then
-	sed "s/s in 100408/s in ${s}/g" < ${SCRIPT}/HCP_preproc.sh > ~/tmp/s${s}.sh
-	qsub -l mem_free=5G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/s${s}.sh
+	sed "s/s in 996782/s in ${s}/g" < ${SCRIPT}/HCP_preproc.sh > ~/tmp/t${s}.sh
+	qsub -l mem_free=5G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/t${s}.sh
 	#fi
 
 done
