8fd6bf92f697e05c0483d45a5b03a2b46f29c6cb
Kai Hwang <kai.hwang@gmail.com>
2017-06-22 13:24:55 -0700

    mimic MAC's preproc.

---
+++ Scripts/HCP_preproc_tha_forMac.sh
@@ -0,0 +1,148 @@
+#do preprocessing for Mac
+
+SOURCE='/home/despoB/connectome-raw'
+WD='/home/despoB/connectome-data'
+SCRIPTS='/home/despoB/kaihwang/bin/HCP-processing'
+Tha_folder='/home/despoB/connectome-thalamus/connectome'
+
+
+for s in 100307; do
+
+	if [ ! -d ${WD}/${s}/ ]; then
+		mkdir ${WD}/${s}/		
+	fi
+
+	if [ ! -e ${WD}/${s}/aseg+tlrc.BRIK ]; then
+		3dcopy ${SOURCE}/${s}/MNINonLinear/aparc+aseg.nii.gz ${WD}/${s}/aseg
+		3drefit -view tlrc ${WD}/${s}/aseg+orig
+	fi
+
+	#whole brain mask
+	if [ ! -e ${WD}/${s}/brainmask_ds.nii.gz ]; then
+		#cp ${Tha_folder}/${s}/MNINonLinear/brainmask_ds.nii.gz ${WD}/${s}/brainmask_ds.nii.gz
+		cp ${SOURCE}/${s}/MNINonLinear/brainmask_fs.nii.gz ${WD}/${s}/brainmask_fs.nii.gz
+
+		3dresample -inset ${WD}/${s}/brainmask_fs.nii.gz \
+		-master ${SOURCE}/${s}/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_hp2000_clean.nii.gz \
+		-prefix ${WD}/${s}/brainmask_ds.nii.gz
+	fi
+
+
+	#csf mask
+	if [ ! -e ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz ]; then
+		3dcalc -a ${WD}/${s}/aseg+tlrc -prefix ${WD}/${s}/CSF_mask.nii.gz -expr 'amongst(a,4,43)'
+		3dresample -inset ${WD}/${s}/CSF_mask.nii.gz  \
+		-master ${WD}/${s}/brainmask_ds.nii.gz \
+		-prefix ${WD}/${s}/CSF_mask_ds.nii.gz
+
+		3dmask_tool -input ${WD}/${s}/CSF_mask_ds.nii.gz -dilate_inputs -1 -prefix ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz
+	fi
+
+	#wm mask
+	if [ ! -e ${WD}/${s}/WM_mask_ds_erode1x.nii.gz ]; then
+		3dcalc -a ${WD}/${s}/aseg+tlrc -prefix ${WD}/${s}/WM_mask.nii.gz -expr 'amongst(a,2,7,16,41,46,251,252,253,254,255)'
+		3dresample -inset ${WD}/${s}/WM_mask.nii.gz  \
+		-master ${WD}/${s}/brainmask_ds.nii.gz \
+		-prefix ${WD}/${s}/WM_mask_ds.nii.gz
+
+		3dmask_tool -input ${WD}/${s}/WM_mask_ds.nii.gz -dilate_inputs -1 -prefix ${WD}/${s}/WM_mask_ds_erode1x.nii.gz
+	fi
+
+
+
+
+	## do task data
+	for data in tfMRI_MOTOR_LR tfMRI_MOTOR_RL tfMRI_WM_LR tfMRI_WM_RL; do
+	
+		if [ ! -d ${WD}/${s}/${data} ]; then
+			mkdir ${WD}/${s}/${data}		
+		fi
+	
+		if [ ! -e ${WD}/${s}/${data}/${data}.nii.gz ]; then
+			rm ${WD}/${s}/${data}/${data}.nii.g
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/${data}.nii.gz ${WD}/${s}/${data}/${data}.nii.gz
+		fi
+		
+		if [ ! -e ${WD}/${s}/${data}/${data}_MACreg.nii.gz ]; then
+
+			rm ${WD}/${s}/${data}/${data}_MACreg.nii.gz
+
+			# extract tissue regressors using 3dpc (compcor)
+			#3dmaskave -mask ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_csf.1D
+			#3dmaskave -mask ${WD}/${s}/WM_mask_ds_erode1x.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_WM.1D
+			#3dmaskave -mask ${WD}/${s}/brainmask_ds.nii.gz -quiet ${WD}/${s}/${data}/${data}.nii.gz > ${WD}/${s}/${data}/${data}_WBS.1D	
+			3dpc -vmean -mask ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz -pcsave 5 -prefix ${WD}/${s}/${data}/CSF_PC ${WD}/${s}/${data}/${data}.nii.gz
+			3dpc -vmean -mask ${WD}/${s}/WM_mask_ds_erode1x.nii.gz -pcsave 5 -prefix ${WD}/${s}/${data}/WM_PC ${WD}/${s}/${data}/${data}.nii.gz
+
+
+			rm ${WD}/${s}/${data}/${data}_motion.1D
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/Movement_Regressors_dt.txt ${WD}/${s}/${data}/${data}_motion.1D
+	
+			#do regression
+			3dTproject \
+			-input ${WD}/${s}/${data}/${data}.nii.gz \
+			-prefix ${WD}/${s}/${data}/${data}_MACreg.nii.gz \
+			-ort ${WD}/${s}/${data}/CSF_PC_vec.1D \
+			-ort ${WD}/${s}/${data}/WM_PC_vec.1D \
+			-ort ${WD}/${s}/${data}/${data}_motion.1D \
+			-automask \
+			-passband 0.0071 0.125
+
+		fi
+
+		3dNetCorr \
+		-inset ${WD}/${s}/${data}/${data}_MACreg.nii.gz \
+		-in_rois /home/despoB/kaihwang/Rest/ROIs/Thalamus_Morel_consolidated_mask_v3.nii.gz \
+		-ts_out \
+		-ts_label \
+		-prefix /home/despoB/kaihwang/bin/HCP-processing/Data/TS/${s}_${data}_thalamus
+
+		rm ${WD}/${s}/${data}/${data}_MACreg.nii.gz
+
+	
+	done  
+
+		## do rfMRI
+	for data in rfMRI_REST1_LR rfMRI_REST1_RL rfMRI_REST2_LR rfMRI_REST2_RL; do
+
+		if [ ! -d ${WD}/${s}/${data} ]; then
+			mkdir ${WD}/${s}/${data}		
+		fi
+
+		if [ ! -e ${WD}/${s}/${data}/${data}.nii.gz ]; then
+			rm ${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/${data}_hp2000_clean.nii.gz ${WD}/${s}/${data}/${data}_hp2000_clean.nii.gz
+			rm ${WD}/${s}/${data}/${data}.nii.gz
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/${data}.nii.gz ${WD}/${s}/${data}/${data}.nii.gz
+		fi
+
+		if [ ! -e ${WD}/${s}/${data}/${data}_MACreg.nii.gz ]; then
+			
+			rm ${WD}/${s}/${data}/${data}_MACreg.nii.gz
+			rm ${WD}/${s}/${data}/${data}_motion.1D
+			ln -s ${SOURCE}/${s}/MNINonLinear/Results/${data}/Movement_Regressors_dt.txt ${WD}/${s}/${data}/${data}_motion.1D
+			3dpc -vmean -mask ${WD}/${s}/CSF_mask_ds_erode1x.nii.gz -pcsave 5 -prefix ${WD}/${s}/${data}/CSF_PC ${WD}/${s}/${data}/${data}.nii.gz
+			3dpc -vmean -mask ${WD}/${s}/WM_mask_ds_erode1x.nii.gz -pcsave 5 -prefix ${WD}/${s}/${data}/WM_PC ${WD}/${s}/${data}/${data}.nii.gz
+
+			3dTproject \
+			-input ${WD}/${s}/${data}/${data}.nii.gz \
+			-prefix ${WD}/${s}/${data}/${data}_MACreg.nii.gz \
+			-ort ${WD}/${s}/${data}/CSF_PC_vec.1D \
+			-ort ${WD}/${s}/${data}/WM_PC_vec.1D \
+			-ort ${WD}/${s}/${data}/${data}_motion.1D \
+			-passband 0.0071 0.125 \
+			-automask
+		fi
+
+		3dNetCorr \
+		-inset ${WD}/${s}/${data}/${data}_MACreg.nii.gz \
+		-in_rois /home/despoB/kaihwang/Rest/ROIs/Thalamus_Morel_consolidated_mask_v3.nii.gz \
+		-ts_out \
+		-ts_label \
+		-prefix /home/despoB/kaihwang/bin/HCP-processing/Data/TS/${s}_${data}_thalamus
+
+		rm ${WD}/${s}/${data}/${data}_MACreg.nii.gz
+	
+	done 
+
+done
--- Scripts/qsub_hcp.sh
+++ Scripts/qsub_hcp.sh
@@ -1,11 +1,13 @@
 SCRIPT='/home/despoB/kaihwang/bin/HCP-processing/Scripts'
 DATA='/home/despoB/kaihwang/bin/HCP-processing/Data'
 
-cd /home/despoB/connectome-raw
-for s in $(/bin/ls -d * | grep "[0-9]"); do
+cd /home/despoB/connectome-data
+for s in $(cat ${DATA}/unrelated.csv); do #(/bin/ls -d *)
+
+
 	#if [ ! -e "/home/despoB/kaihwang/Rest/Graph/gsetCI_${Subject}.mat" ]; then
-	sed "s/s in 996782/s in ${s}/g" < ${SCRIPT}/HCP_preproc.sh > ~/tmp/t${s}.sh
-	qsub -l mem_free=5G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/t${s}.sh
+	sed "s/s in 100307/s in ${s}/g" < ${SCRIPT}/HCP_preproc_tha_forMac.sh > ~/tmp/HCP${s}.sh
+	qsub -l mem_free=11G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/HCP${s}.sh
 	#fi
 
 done
