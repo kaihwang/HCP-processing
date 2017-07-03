dc66e0dd92c07f554c0f18848c65f57c83316091
Kai Hwang <kai.hwang@gmail.com>
2017-07-03 11:37:49 -0700

    change fn.

--- Scripts/qsub_hcp.sh
+++ Scripts/qsub_hcp.sh
@@ -6,7 +6,7 @@ for s in $(cat ${DATA}/unrelated.csv); do #(/bin/ls -d *)
 
 
 	#if [ ! -e "/home/despoB/kaihwang/Rest/Graph/gsetCI_${Subject}.mat" ]; then
-	sed "s/s in 100307/s in ${s}/g" < ${SCRIPT}/HCP_preproc_tha_forMac.sh > ~/tmp/HCP${s}.sh
+	sed "s/s in 100307/s in ${s}/g" < ${SCRIPT}/HCP_preproc_forMac.sh > ~/tmp/HCP${s}.sh
 	qsub -l mem_free=11G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/HCP${s}.sh
 	#fi
 
